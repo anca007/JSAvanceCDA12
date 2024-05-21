0ad98302ff63346172850d76344675cd2c3cc8ad
stropee <stropee@campus-eni.fr>
2024-05-21 11:36:49 +0200

    demo 1

---
+++ Module_1/demo1/script.js
@@ -0,0 +1,48 @@
+let pinguin = "Pingouin"
+let polarBear = 'Ours polaire'
+
+let toDay = new Date()
+
+let locations = ["banquise", "batman", "madagascar", "l'age de glace"];
+
+// console.log(pinguin)
+// console.log(typeof pinguin)
+// console.log(locations)
+
+//boucles
+// for (const location of locations) {
+//     console.log(location)
+// }
+//
+// let morse = {
+//     name: "MorseAttack",
+//     age: 12,
+//     gender: "M"
+// }
+//
+// for (const morseKey in morse) {
+//     console.log(morseKey)
+//     console.log(morse[morseKey])
+// }
+
+// locations.forEach(function (value, idx) {
+//     console.log(value)
+// })
+//
+// locations.forEach(val => {
+//     console.log(val + "lambda")
+// })
+
+const hell = function () {
+    console.log("Hell World !")
+};
+
+
+(function () {
+    console.log("Hello world !")
+})()
+
+
+
+
+
