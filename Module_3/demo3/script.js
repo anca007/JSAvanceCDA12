d619f073db662c6b326fbf67bcfe7d7f7f17f0f7
stropee <stropee@campus-eni.fr>
2024-05-23 16:36:27 +0200

    module 3 API demo async await

---
+++ Module_3/demo3/script.js
@@ -0,0 +1,124 @@
+// let pikachu;
+//
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//     .then(response => response.json())
+//     .then(data => {
+//             pikachu = data
+//             console.log(pikachu)
+//             let p = document.createElement('p')
+//             p.innerText = pikachu.name
+//
+//             let img = document.createElement('img')
+//             img.src = pikachu.sprites.other.home.front_default
+//
+//             document.getElementById('pokemon').append(p)
+//             document.getElementById('pokemon').append(img)
+//         }
+//     ).catch(error => console.log(error))
+//
+// console.log(pikachu)
+
+
+function synchrone() {
+    let i = 0;
+    while (i < 3_000_000_000) {
+        i++
+    }
+    return "Fin de calcul"
+}
+
+function promesse() {
+    return Promise.resolve()
+        .then(synchrone)
+}
+
+// console.log("avant")
+// promesse().then(retour => console.log(retour))
+// console.log("après")
+
+//async
+const getSki = (number) => {
+
+    const skis = {
+        1: 'alpin',
+        2: 'nautique',
+        3: 'fond',
+        4: 'randonnée'
+    }
+
+    return Promise.resolve(skis[number])
+}
+//getSki(2).then(data => console.log(data))
+
+const getSkiAsync = async (number) => {
+
+    const skis = {
+        1: 'alpin',
+        2: 'nautique',
+        3: 'fond',
+        4: 'randonnée'
+    }
+
+    let i = 0;
+    while (i < 1_000_000_000) {
+        i++
+    }
+    console.log("------------FIN---------------", number)
+    return skis[number]
+}
+
+//getSkiAsync(3).then(data => console.log(data))
+
+
+//await
+
+const sportDeSki = async () => {
+
+    const alpin = getSkiAsync(1)
+    const rando = getSkiAsync(2)
+    const tout = await Promise.all([alpin, rando])
+
+    return tout
+}
+
+const sportDeSki2 =  async () => {
+
+    const alpin = await getSkiAsync(3)
+    const rando = await getSkiAsync(4)
+
+    let promise = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+
+    if(promise.status == 200){
+
+    }
+
+
+
+    return [alpin, rando]
+}
+
+sportDeSki().then(console.log)
+sportDeSki2().then(console.log)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
